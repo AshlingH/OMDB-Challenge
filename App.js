c5b17da99b5707b2bb4f8faf6246d26403375290
Ashling <hennessyaisling9@gmail.com>
2020-09-03 19:54:48 -0700

    Set up form and basic layout

--- App.js
+++ App.js
@@ -1,4 +1,27 @@
 import React from "react";
 
 
+    <form onSubmit={e => handleSubmit(e)}>
+        <div><h1>Search for a Movie Title</h1></div>
+        <img src="/img/005-shopping-basket.png" alt="Shopping List Basket" />
+        <div>
+
+        <input
+            name="name"
+            type="text"
+            placeholder="ie: movie title"
+        />
+        </div>
+        <Button
+            node="button"
+            type="submit"
+            waves="light"
+        >
+            Submit
+                    <Icon right>
+                send
+                    </Icon>
+        </Button>
+    </form>
+
 export default App;
---
+++ Public/Footer.js
@@ -0,0 +1,19 @@
+import React from "react;
+
+
+const defaultFooter = (
+
+
+
+
+return (
+    <Fragment>
+        <footer className="footer">
+            <h1> Project Complete by Aisling Hennessy</h1>
+        </footer>
+    </Fragment>
+);
+
+
+
+export default Footer;
